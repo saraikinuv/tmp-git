c9ed67bd78068d83046827cd34369015ed0bb816
saraikinuv <saraikinuv@gmail.com>
2023-12-20 17:01:10 +0300

    изменен tmp

--- script/tmp.R
+++ script/tmp.R
@@ -4,3 +4,15 @@ library(tidyverse)
 
 mtcars %>% 
   summarise(across(where(is.numeric), mean), by = cyl)
+
+df <- clipr::read_clip_tbl()
+
+df %>% 
+  mutate(across(starts_with("sp"), ~.x >= 2))
+
+df %>% 
+  mutate(n = rowSums(pick(starts_with("sp")) >= 2, na.rm = T))
+library(data.table)
+
+COLS <- grep("sp", names(df))
+setDT(df)[, n := rowSums(.SD >= 2), .SDcols = COLS][]
